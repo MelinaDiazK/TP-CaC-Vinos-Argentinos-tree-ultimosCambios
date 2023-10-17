314d020fd176235f00e5138a371add9c79fdfc4c
MelinaDiazK <89316166+MelinaDiazK@users.noreply.github.com>
2023-10-17 17:38:10 -0300

    Delete TP-CaC-Vinos-Argentinos directory

--- TP-CaC-Vinos-Argentinos/java-js/proyectos.js
+++
@@ -1,7 +0,0 @@
-function accion (){
-    console.log ('boton ok');
-    var botones = document.getElementsByClassName ('bton');
-    for (var i= 0; i < botones.length; i++) {
-        botones[i].classList.toggle('desaparece');
-    }
-}
